b0a92ed11507537258bd4f8b2280d223175dde7c
Taylor Ambrose <taykesby@gmail.com>
2023-03-13 16:39:20 -0400

    python functions lab completed

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
+# function greetProgrammer() {
+#   console.log("Hello, programmer!");
+# }
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
+
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+
 
 def halve(number):
-    pass
+    return number / 2
